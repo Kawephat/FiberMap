e23cbee4710590d45a160d187b0c34b80b6d71ee
Kawephat <Kawephat_w@cmu.ac.th>
2017-11-02 19:39:10 +0700

    vuefix

--- src/App.vue
+++ src/App.vue
@@ -84,10 +84,10 @@
       }
 
     },
-    created: function () {
-    this.axios.get('http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1').then((response) => {
-  console.log(response.data)
-})
-  }
+  //   created: function () {
+  //   this.axios.get('http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1').then((response) => {
+  // console.log(response.data)
+// })
+  // }
   }
 </script>
